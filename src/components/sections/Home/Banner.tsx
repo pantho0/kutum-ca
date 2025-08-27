a859ab242aeccaf8c13febcea8ad640aa58f955a
pantho0 <panthomashreky6@gmail.com>
2025-08-27 23:12:10 +0600

    updated banner design

--- src/components/sections/Home/Banner.tsx
+++ src/components/sections/Home/Banner.tsx
@@ -3,22 +3,31 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import heroImg from "../../../../public/images/hero-1.png";
 
+import { useLocale, useTranslations } from "next-intl";
+
 const Banner = () => {
+  const t = useTranslations("Homepage");
+  const locale = useLocale();
   return (
-    <section className="w-full text-white ">
-      <div className="container relative mx-auto  min-h-screen max-w-6xl items-center px-4 py-12 lg:flex-row lg:justify-between lg:px-8 space-y-6">
+    <section className="w-full text-white min-h-dvh relative ">
+      <div className="container mx-auto  min-h-screen max-w-6xl items-center px-4 py-12 lg:px-8 space-y-6">
         <div>
-          <h2 className="text-9xl font-elsie">Perfectly Light In Every Bite</h2>
+          <h2
+            className={`font-elsie w-[90%] z-50 ${
+              locale === "fr" ? "text-7xl" : "text-8xl"
+            }`}
+          >
+            {t("BannerTitle")}
+          </h2>
         </div>
         <div className="flex items-center">
-          <div className="mr-4 h-px w-16 bg-[#F5DEB3]"></div>
+          <div className="mr-4 h-px w-26 bg-[#F5DEB3]"></div>
           <p className="text-lg font-sans font-light max-w-1/2">
-            We have a proper passion for cooking. Love is the secret ingredient
-            that makes all our meals taste better and magical.
+            {t("BannerDescription")}
           </p>
-          <div className="absolute bottom-0 right-10">
-            <Image src={heroImg} alt="hero" />
-          </div>
+        </div>
+        <div className="absolute top-1/2 right-26 transform -translate-y-1/2">
+          <Image src={heroImg} alt="hero" />
         </div>
       </div>
     </section>
