f4d5967c890489a0ded1e9e46e55f8fdcc3f3e6c
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-09-10 23:29:42 +0600

    added categories adding service and hooks

--- src/hooks/category.hook.ts
+++ src/hooks/category.hook.ts
@@ -1,9 +1,23 @@
-import { getAllCategory } from "@/services/Category";
-import { useQuery } from "@tanstack/react-query";
+import { addCategory, getAllCategory } from "@/services/Category";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export const useGetCategory = () => {
   return useQuery({
     queryKey: ["categories"],
     queryFn: async () => await getAllCategory(),
+  });
+};
+
+export const useAddCategory = () => {
+  return useMutation({
+    mutationKey: ["category"],
+    mutationFn: async (categoryData: any) => await addCategory(categoryData),
+    onSuccess: () => {
+      toast.success("Category added successfully", { duration: 2000 });
+    },
+    onError: () => {
+      toast.error("Failed to add category", { duration: 2000 });
+    },
   });
 };
--- src/services/Category/index.ts
+++ src/services/Category/index.ts
@@ -1,5 +1,6 @@
 "use server";
 import axiosInstance from "@/lib/AxiosInstance";
+import { revalidateTag } from "next/cache";
 
 export const getAllCategory = async () => {
   try {
@@ -11,6 +12,22 @@ export const getAllCategory = async () => {
 
     return res.data;
   } catch (error: any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Error fetching data"
+    );
+  }
+};
+
+export const addCategory = async (categoryData: any) => {
+  try {
+    const res = await axiosInstance.post("/categories", categoryData);
+    if (!res.data.success) {
+      throw new Error(res.data.message || "Error adding category");
+    }
+    revalidateTag("categories");
+    return res.data;
+  } catch (error: any) {
+    console.log(error);
     throw new Error(
       error.response?.data?.message || error.message || "Error fetching data"
     );
